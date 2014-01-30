5eee4ce2aa95659456e8145c6a753a22414a16e8
Jared Hanson <jaredhanson@gmail.com>
2014-01-29 19:52:07 -0800

    Add test cases.

--- test/exchange/chain.test.js
+++ test/exchange/chain.test.js
@@ -48,6 +48,40 @@ describe('exchange.chain', function() {
     });
   });
   
+  describe('issuing an access token and refresh token', function() {
+    var response, err;
+
+    before(function(done) {
+      function issue(client, token, done) {
+        if (client.id == 'c123' && token == 'shh') {
+          return done(null, 's3cr1t', 'getANotehr')
+        }
+        return done(new Error('something is wrong'));
+      }
+      
+      chai.connect.use(chain(issue))
+        .req(function(req) {
+          req.user = { id: 'c123' };
+          req.body = { oauth_token: 'Bearer shh' };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"s3cr1t","refresh_token":"getANotehr","token_type":"Bearer"}');
+    });
+  });
+  
   describe('issuing an access token and params', function() {
     var response, err;
 
@@ -113,6 +147,74 @@ describe('exchange.chain', function() {
     
     it('should respond with body', function() {
       expect(response.body).to.equal('{"access_token":"s3cr1t","token_type":"foo","expires_in":3600}');
+    });
+  });
+  
+  describe('issuing an access token, null refresh token, and params', function() {
+    var response, err;
+
+    before(function(done) {
+      function issue(client, token, done) {
+        if (client.id == 'c123' && token == 'shh') {
+          return done(null, 's3cr1t', null, { 'expires_in': 3600 })
+        }
+        return done(new Error('something is wrong'));
+      }
+      
+      chai.connect.use(chain(issue))
+        .req(function(req) {
+          req.user = { id: 'c123' };
+          req.body = { oauth_token: 'Bearer shh' };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"s3cr1t","expires_in":3600,"token_type":"Bearer"}');
+    });
+  });
+  
+  describe('issuing an access token, refresh token, and params with token type', function() {
+    var response, err;
+
+    before(function(done) {
+      function issue(client, token, done) {
+        if (client.id == 'c123' && token == 'shh') {
+          return done(null, 's3cr1t', 'blahblag', { 'token_type': 'foo', 'expires_in': 3600 })
+        }
+        return done(new Error('something is wrong'));
+      }
+      
+      chai.connect.use(chain(issue))
+        .req(function(req) {
+          req.user = { id: 'c123' };
+          req.body = { oauth_token: 'Bearer shh' };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"s3cr1t","refresh_token":"blahblag","token_type":"foo","expires_in":3600}');
     });
   });
   
